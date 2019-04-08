7b5e356a653cc6b685a81f613f39e50611a0f72a
Michael <mdaniels6532@gmail.com>
2019-04-08 17:27:14 -0400

    first commit for Angular mad-libs exercise repository

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mad-libs';
+  appInput: string=null;
+  otherInput: string=null;
+  userName: string=null;
+  hideTitle: boolean = true;
 }
+
